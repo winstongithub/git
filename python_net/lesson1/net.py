9f8b1238c72ace57c87f6c2e25cb2a96d8a17d65
Your Name <you@example.com>
2016-05-03 20:04:25 -0700

    catch except

--- python_net/lesson1/net.py
+++ python_net/lesson1/net.py
@@ -3,7 +3,11 @@ port = 70
 host = sys.argv[1]
 filename = sys.argv[2]
 s = socket.socket( socket.AF_INET , socket.SOCK_STREAM )
-s.connect( (host , port) )
+try:
+	s.connect( (host , port) )
+except socket.gaierror , e:
+	print (" Error connecting to server: %s ", e )
+	sys.exit(1)
 s.sendall( filename + "\r\n")
 while 1:
 	buf = s.recv( 2048 )
