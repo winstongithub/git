0eb0483ac714bbe79f0b52ed0c5f699d481a1793
winstongithub <670293316@qq.com>
2016-08-30 17:58:47 +0800

    add lookat

--- pyopengl/test.py
+++ pyopengl/test.py
@@ -19,19 +19,20 @@ def transle( x=0 , y=0 , z=0 ):
     mat4[2][3]=z
     return mat4
 
-def rotate( x=None , y=None , z=None ):
+def rotate( angle , x=0 , y=0 , z=0 ):
     mat4=np.eye(4)
-    if x != None:
-        mat4= np.dot(np.array([[ 1,0,0,0],[0,np.cos(x),-np.sin(x),0]
-                      ,[0,np.sin(x),np.cos(x),0],[0,0,0,1]],float),mat4)
-    if y != None:
-        mat4= np.dot(np.array([[ np.cos(y),0,np.sin(y),0],[0,1,0,0]
-                      ,[-np.sin(y),0,np.cos(y),0],[0,0,0,1]],float),mat4)
-
-    if z != None:
-        mat4= np.dot(np.array([[ np.cos(z),-np.sin(x),0,0],[np.sin(z),np.cos(x),0,0]
-                      ,[0,0,1,0],[0,0,0,1]],float),mat4)
-    return mat4
+    if (x+y+z)==0:
+        return mat4
+    else:
+        c= np.cos(angle)
+        s=np.sin(angle)
+        rotaMat4 = np.array([[ x**2*(1-c)+c , x*y*(1-c)-z*s , x*z*(1-c)+y*s , 0],
+                             [x*y*(1-c)+z*s , y**2*(1-c)+c , y*z*(1-c)-x*s , 0],
+                             [x*z*(1-c)-y*s , y*z*(1-c)+x*s , z**2*(1-c)+c , 0 ],
+                             [0 , 0 , 0 ,1]
+                             ],float)
+        
+    return rotaMat4
                      
 def scale( x=1 , y=1 ,z=1 ):
     mat4=np.eye(4)
@@ -45,14 +46,46 @@ vertexbuffer = 0;
 def runModelTrans( g):
     array = transToArray( g , 3 )
     transMat = transle(x=0.4 , y=0.4,z=0.4)
-    transMat = np.dot(rotate(np.pi,np.pi,np.pi) , transMat)
+    transMat = np.dot(rotate(np.pi,0,0,1) , transMat)
     transMat = np.dot( scale( 0.4 ,0.4 ,0.4 ) , transMat)
+    transMat = np.dot( setLookAtMat( 0 , 0 , 0 , 0,0,1, 0 ,1,0) ,transMat )
     newList=[]
     for i in range( 0 , len(array)):
         perArray = np.dot( transMat , array[i])
         newList.append( perArray.tolist())
     return np.array(newList,float )
-    
+
+def norMalize( v ):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    else:
+        return v/norm
+
+def setLookAtMat( eyex ,eyey , eyez , centerx , centery ,centerz , upx , upy , upz):
+    p = np.array([ eyex , eyey ,eyez , 1 ],float )
+    l = np.array([ centerx , centery , centerz , 1 ],float)
+    u = np.array([upx,upy,upz,0],float)
+    d = l-p
+    r = np.cross( u[0:3] , d[0:3] )
+    u=norMalize(u)
+    d=norMalize(d)
+    r=norMalize(r)
+    r=r.tolist()
+    r.append(0)
+    r = np.array(r,float)
+
+    x = -np.dot( p , r )
+    y = -np.dot( p , u )
+    z = -np.dot(p ,d )
+
+    r[3] = x
+    u[3] = y
+    d[3] = z
+    mat4= np.array([ r , u , d ,[ 0 , 0 , 0 , 1 ] ], float)
+    return mat4.transpose()
+
+
 def init():
     global vertexbuffer
     vertexbuffer = glGenBuffers(1)
