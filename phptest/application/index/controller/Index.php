2848dd5dc29e631cd6ab174746398cbac42c1bec
winston <670293316@qq.com>
2017-12-27 20:26:04 +0800

    上传文件

--- phptest/application/index/controller/Index.php
+++ phptest/application/index/controller/Index.php
@@ -5,9 +5,13 @@ use think\Controller;
 use think\Db;
 class Index extends Controller
 {
-public function index()
-{
-$data = Db::name('data')->select();
-return json_encode($data);
-}
+    public function index()
+    {
+        $data = Db::name('data')->select();
+        return $this->fetch();
+    }
+    public function upload_file( $form )
+    {
+        return json_encode($form);
+    }
 }
