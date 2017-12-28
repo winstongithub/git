e295ea7a07c6016c8f9ed4ae0c489820ef16c20a
winston <670293316@qq.com>
2017-12-28 19:21:28 +0800

    列出目录下文件

--- phptest/application/index/controller/Index.php
+++ phptest/application/index/controller/Index.php
@@ -14,4 +14,33 @@ class Index extends Controller
     {
         return json_encode($form);
     }
+    public function down_load()
+    {
+       
+    }
+    public function show_dir()
+    {
+        $filepath ='C:\Users\Administrator\Desktop\nntg\data';
+        if (is_dir($filepath)) {                //检测是否是一个目录
+            $dir =scandir($filepath);
+            $ret = Array();
+            foreach ($dir as $value){
+                $curpath = $filepath . "\\" . $value;
+                if (is_dir($curpath))
+                    continue;
+                $fileinfo = Array("name"=>""); //创建信息数组
+                $handle = fopen($curpath,'r');
+                $fstat = fstat($handle);
+                $fileinfo['filename'] = $value;
+                $fileinfo['size'] = round($fstat["size"]/1024,2);
+                array_push($ret,$fileinfo);
+            }
+            $this->assign("ret",$ret);
+            return $this->fetch();
+        }
+        else
+        {
+            return "error";
+        }
+    }
 }
